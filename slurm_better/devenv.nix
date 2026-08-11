ca6ad9de9ef19333060429d3b402a2d521be4daf
wolfbene <benedikt.wolf@uni-a.de>
2026-08-11 22:34:14 +0200

    hier nicht hinschauen ist peinlich

--- slurm_better/devenv.nix
+++ slurm_better/devenv.nix
@@ -29,17 +29,6 @@
         };
     };
 
-    services.postgres = {
-    enable = true;
-    package = pkgs.postgresql_16; # Definiert die Version
-    initialDatabases = [
-      { name = "optuna_db"; }
-    ];
-    # Erstellt einen lokalen Socket im Projektverzeichnis, um Netzwerkkonflikte zu vermeiden
-    listen_addresses = "127.0.0.1"; 
-  };
-
-
   env.LD_LIBRARY_PATH = lib.makeLibraryPath [
     pkgs.stdenv.cc.cc.lib
     pkgs.zlib
