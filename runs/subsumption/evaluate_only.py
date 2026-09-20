b91bfaaca58ee415b9f3dfa72a3819bce872f0d6
wolfbene <benedikt.wolf@uni-a.de>
2026-09-20 13:03:47 +0200

    setup

--- runs/subsumption/evaluate_only.py
+++ runs/subsumption/evaluate_only.py
@@ -91,7 +91,7 @@ def run(problem: str, job_id: str, optimizer: str, worker_id: int):
             ),
             mutation=mutation.HalfnormIncrease(),
             origin_generation=origin.SquaredError(),
-            #subsumption=PreferSmallerVolume(tolerance=0.01),
+            subsumption=PreferSmallerVolume(tolerance=0.01),
         ),
         solution_composition=opt_dict[optimizer](n_iter=32, population_size=32),
         n_iter=200,
@@ -102,7 +102,7 @@ def run(problem: str, job_id: str, optimizer: str, worker_id: int):
         early_stopping_patience=5,
         early_stopping_delta=0.0015,
         extra_rules_patience=1,
-        extra_rules_delta=0.1000,
+        extra_rules_delta=0.0100,
     )
 
     
